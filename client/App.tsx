642a256cee47f3c76e5e664d8395b7927db3db97
Michael Ng <lunalescia@gmail.com>
2022-08-19 15:30:36 -0700

    adf

--- client/App.tsx
+++ client/App.tsx
@@ -3,38 +3,39 @@ import "./stylesheets/App.scss";
 
 const App = ():JSX.Element  => {
 
-  const [anime, setAnime] = useState({});
-  const [image, setImage] = useState('');
+  // const [anime, setAnime] = useState({});
+  // const [image, setImage] = useState('');
 
-  useEffect(() => {
-    fetch('https://animechan.vercel.app/api/random')
-      .then(response => response.json())
-      .then((anime: animeType) => {
-        setAnime(anime);
-        fetch('/api/search', {
-          method: 'POST',
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(anime)
-        })
-          .then(response => response.json())
-          .then((imgUrl: string) => {
-            setImage(imgUrl);
-          });
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch('https://animechan.vercel.app/api/random')
+  //     .then(response => response.json())
+  //     .then((anime: animeType) => {
+  //       setAnime(anime);
+  //       fetch('/api/search', {
+  //         method: 'POST',
+  //         headers: {
+  //           "Content-Type": "application/json"
+  //         },
+  //         body: JSON.stringify(anime)
+  //       })
+  //         .then(response => response.json())
+  //         .then((imgUrl: string) => {
+  //           setImage(imgUrl);
+  //         });
+  //     });
+  // }, []);
 
   return(
     <div className="app">
-      <div className="info">
+      {/* <div className="info">
         <h1>{anime.anime}</h1>
         <h2>{anime.quote}</h2>
         <h3>{anime.character && '-'+anime.character}</h3>
       </div>
       <div className="image">
         <img src={image}/>
-      </div>
+      </div> */}
+      <h1>Hello</h1>
     </div>
   )
 }
