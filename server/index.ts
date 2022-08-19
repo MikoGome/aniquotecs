143b2761f4d1cee1a563247e20ac05d3e3926268
Michael Ng <lunalescia@gmail.com>
2022-08-19 15:17:53 -0700

    asdfkj

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response, NextFunction} from "express";
+import express, {Request, Response} from "express";
 import webscrape from './webscraping';
 import path from "path";
 
