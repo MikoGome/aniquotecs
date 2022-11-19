606f0b0cb30e9fcff0cd57120de87abaa7f69c67
Michael Ng <lunalescia@gmail.com>
2022-11-19 04:24:19 -0800

    asdf

--- server/webscraping.ts
+++ server/webscraping.ts
@@ -9,12 +9,14 @@ async function webscrape(params: string): Promise<string> {
   });
   const page = await browser.newPage();
   
-  const URL:string =  `https://www.bing.com/images/search?q=${params}&qft=+filterui:imagesize-large&form=IRFLTR&first=1&tsc=ImageHoverTitle`;
+  const URL:string =  `https://www.google.com/search?q=${params}&sxsrf=ALiCzsZZMmZeyExPb_rUvhzUt89XmXM0jw:1668859139397&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiE_7vhmLr7AhVXJUQIHWHfAzYQ_AUoAXoECAIQAw&biw=1920&bih=937&dpr=1`;
   console.log(URL);
   await page.goto(URL);
-  await page.waitForSelector('li[data-idx="1"] img.mimg');
+  await page.waitForSelector('img.rg_i');
+  page.click('img.rg_i');
+  await page.waitForSelector('.pxAole div.v4dQwb img');
   const imgUrl:string = await page.evaluate(() => {
-    const imageEl = document.querySelector('li[data-idx="1"] img.mimg') as HTMLImageElement;
+    const imageEl = document.querySelector('.pxAole div.v4dQwb img') as HTMLImageElement;
     return imageEl.getAttribute('src') as string;
   });
   
