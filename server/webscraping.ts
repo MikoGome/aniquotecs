5bee165a7f6117393efe7a1cb6cbe9fda9033b24
Michael Ng <lunalescia@gmail.com>
2022-11-19 02:39:02 -0800

    adf

--- server/webscraping.ts
+++ server/webscraping.ts
@@ -11,7 +11,7 @@ async function webscrape(params: string): Promise<string> {
   
   const URL:string =  `https://www.bing.com/images/search?q=${params}&qs=n&form=QBIR&qft=+filterui%3Aimagesize-large+filterui%3Aphoto-transparent&sp=-1&pq=${params}&sc=0-27&cvid=900E0B8009244C0DA2F10FC844099845&ghsh=0&ghacc=0&first=1&tsc=ImageHoverTitle&cw=1177&ch=920`;
   await page.goto(URL);
-  await page.waitForSelector('img.mimg');
+  // await page.waitForSelector('img.mimg');
   const imgUrl:string = await page.evaluate(() => {
     const imageEl = document.querySelector('img.mimg') as HTMLImageElement;
     return imageEl.getAttribute('src') as string;
