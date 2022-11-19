109384d9b723377af00424ce5058c2eedd11732e
Michael Ng <lunalescia@gmail.com>
2022-11-19 02:35:03 -0800

    asdkfj

--- server/webscraping.ts
+++ server/webscraping.ts
@@ -9,7 +9,7 @@ async function webscrape(params: string): Promise<string> {
   });
   const page = await browser.newPage();
   
-  const URL:string = `https://www.bing.com/images/search?q=${params}&qft=+filterui%3aimagesize-large&form=AWIR&first=1&tsc=ImageHoverTitle`;
+  const URL:string =  `https://www.bing.com/images/search?q=${params}&qs=n&form=QBIR&qft=+filterui%3Aimagesize-large+filterui%3Aphoto-transparent&sp=-1&pq=${params}&sc=0-27&cvid=900E0B8009244C0DA2F10FC844099845&ghsh=0&ghacc=0&first=1&tsc=ImageHoverTitle&cw=1177&ch=920`;
   await page.goto(URL);
   await page.waitForSelector('img.mimg');
   const imgUrl:string = await page.evaluate(() => {
