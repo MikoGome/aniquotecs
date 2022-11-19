0618301b15c3c92eb8c2e2ce6809c9f08a052d61
Michael Ng <lunalescia@gmail.com>
2022-11-19 02:48:19 -0800

    fss

--- server/webscraping.ts
+++ server/webscraping.ts
@@ -1,7 +1,6 @@
 import puppeteer from 'puppeteer';
 
 async function webscrape(params: string): Promise<string> {
-  console.log(params);
   const browser = await puppeteer.launch({
     'args' : [
       '--no-sandbox',
@@ -12,9 +11,9 @@ async function webscrape(params: string): Promise<string> {
   
   const URL:string =  `https://www.bing.com/images/search?q=${params}&qs=n&form=QBIR&qft=+filterui%3Aimagesize-large+filterui%3Aphoto-transparent&sp=-1&pq=${params}&sc=0-27&cvid=900E0B8009244C0DA2F10FC844099845&ghsh=0&ghacc=0&first=1&tsc=ImageHoverTitle&cw=1177&ch=920`;
   await page.goto(URL);
-  // await page.waitForSelector('img.mimg');
+  await page.waitForSelector('li[data-idx="1"] img.mimg');
   const imgUrl:string = await page.evaluate(() => {
-    const imageEl = document.querySelector('img.mimg') as HTMLImageElement;
+    const imageEl = document.querySelector('li[data-idx="1"] img.mimg') as HTMLImageElement;
     return imageEl.getAttribute('src') as string;
   });
   
